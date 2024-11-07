d5a13230421da376cb05d4cad97579f5d29b20e2
WoodenBrick1 <brickwooden@gmail.com>
2024-11-07 17:36:18 +0100

    finished the books and started on the new book button

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+const libraryMain = document.getElementById("library");
+
 const library = [];
 
 
@@ -6,8 +9,9 @@ function Book(title, author, pages, read)
 {
     this.title = title;
     this.author = author;
-    this.read = read;
     this.pages = pages;
+    this.read = read;
+
 }
 
 function addBook(book)
@@ -15,10 +19,40 @@ function addBook(book)
     library.push(book)
 }
 
-const book1 = new Book("harry potter", "J.K Rowling", "Yes", 130);
+const book1 = new Book("Harry Potter", "J.K Rowling", 130, true);
+const book2 = new Book("1984", "George Orweel", 203, true);
+const book3 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 180, false)
+const book4 = new Book("To Kill a Mockingbird", "Harper Lee", 285, false)
 
 
 addBook(book1);
+addBook(book2);
+addBook(book3);
+addBook(book4);
 
+function displayBooks()
+{
 
-console.log(library);
+    for (let book of library)
+    {
+        const {title, author, pages, read} = book;
+        console.log(book);
+        libraryMain.innerHTML += 
+        `<div class="book">
+        <div class="stripes">
+            <div class="stripe"></div>
+            <div class="stripe"></div>   
+        </div>
+
+        <div class="info">
+            <div class="name"><p>Name:</p>${title}</div>
+            <div class="author"><p>Author:</p>${author}</div>
+            <div class="pages"><p>Pages:</p>${pages}</div>
+            <div class="read"><p>Read?:</p>${read ? "Yes" : "No"}</div>
+        </div>
+    </div>`;
+    }
+    
+}
+
+displayBooks();
